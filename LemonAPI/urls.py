f8571b3695aecb320eac16c36ce8943579a82c0b
Ali Noor <alinoor3865@gmail.com>
2026-08-11 18:56:32 +0500

    Refactor import statements in urls.py

--- LemonAPI/urls.py
+++ LemonAPI/urls.py
@@ -1,16 +1,16 @@
 from django.urls import path
-from .views import *
+from . import views
 
 urlpatterns = [
-    path('categories', CategoriesView.as_view()),
-    path('menu-items', MenuItemsView.as_view()),
-    path('menu-items/<int:pk>', SingleMenuItemView.as_view()),
-    path('cart/menu-items', CartView.as_view()),
-    path('orders', OrderView.as_view()),
-    path('orders/<int:pk>', SingleOrderView.as_view()),
-    path('groups/manager/users', GroupViewSet.as_view(
+    path('categories', views.CategoriesView.as_view()),
+    path('menu-items', views.MenuItemsView.as_view()),
+    path('menu-items/<int:pk>', views.SingleMenuItemView.as_view()),
+    path('cart/menu-items', views.CartView.as_view()),
+    path('orders', views.OrderView.as_view()),
+    path('orders/<int:pk>', views.SingleOrderView.as_view()),
+    path('groups/manager/users', views.GroupViewSet.as_view(
         {'get': 'list', 'post': 'create', 'delete': 'destroy'})),
 
-    path('groups/delivery-crew/users', DeliveryCrewViewSet.as_view(
+    path('groups/delivery-crew/users', views.DeliveryCrewViewSet.as_view(
         {'get': 'list', 'post': 'create', 'delete': 'destroy'}))
 ]
