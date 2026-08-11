eba6c85ed4724d3250ef9be8a51e507a0530521c
Ali Noor <alinoor3865@gmail.com>
2026-08-11 18:57:10 +0500

    Fix API URL include path in urls.py

--- LittleLemonAPI/urls.py
+++ LittleLemonAPI/urls.py
@@ -17,9 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from django.contrib import admin
+from django.urls import path, include
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('LittleLemonAPI.urls')),
+    path('api/',include('LemonAPI.urls')),
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.authtoken')),
 ]
