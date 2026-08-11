f077e4910707bbf40aa977d8adae63e5570f9491
Ali Noor <alinoor3865@gmail.com>
2026-08-11 18:54:44 +0500

    Refactor REST framework settings in settings.py
    Removed XMLRenderer and DjangoFilterBackend from REST framework settings. Updated PAGE_SIZE from 5 to 2.

--- LittleLemonAPI/settings.py
+++ LittleLemonAPI/settings.py
@@ -122,24 +122,14 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 REST_FRAMEWORK = {
-    'DEFAULT_RENDERER_CLASSES': (
-        'rest_framework.renderers.JSONRenderer',
-        'rest_framework.renderers.BrowsableAPIRenderer',
-        'rest_framework_xml.renderers.XMLRenderer',
-    ),
+    'DEFAULT_FILTER_BACKENDS': [
+        'rest_framework.filters.OrderingFilter',
+        'rest_framework.filters.SearchFilter',
+    ],
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
         'rest_framework.authentication.SessionAuthentication',
     ),
-    'DEFAULT_FILTER_BACKENDS': (
-        'rest_framework.filters.OrderingFilter',
-        'rest_framework.filters.SearchFilter',
-        'django_filters.rest_framework.DjangoFilterBackend'
-    ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 5,
-}
-
-DJOSER = {
-    "USER_ID_FIELD": "username",
+    'PAGE_SIZE': 2
 }
